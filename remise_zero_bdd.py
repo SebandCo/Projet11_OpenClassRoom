99a71a2da36c708c81a4d3258c5ce5d8a8b666c2
SebandCo <113468072+SebandCo@users.noreply.github.com>
2023-09-03 19:35:54 +0200

    Correction sur les festivals avec une date dépassée

--- remise_zero_bdd.py
+++ remise_zero_bdd.py
@@ -6,14 +6,14 @@ import os
 def remise_zero_bdd():
     # Remise à zéro base de données clubs
     if os.path.exists("clubs.json"):
-        with open("original_clubs.json","r") as fichier:
+        with open("original_clubs.json", "r") as fichier:
             data = fichier.read()
         with open("clubs.json", "w") as fichier:
             fichier.write(data)
-    
+
     # Remise à zéro base de données competitions
     if os.path.exists("competitions.json"):
-        with open("original_competitions.json","r") as fichier:
+        with open("original_competitions.json", "r") as fichier:
             data = fichier.read()
         with open("competitions.json", "w") as fichier:
             fichier.write(data)
--- server.py
+++ server.py
@@ -1,32 +1,47 @@
 import json
-from flask import Flask,render_template,request,redirect,flash,url_for
+from flask import Flask, render_template, request, redirect, flash, url_for
 from remise_zero_bdd import remise_zero_bdd
+from datetime import datetime
 
 ATHLETES_MAX_COMPETITION = 12
+FORMAT_DATE = "%Y-%m-%d %H:%M:%S"
 
 app = Flask(__name__)
 app.secret_key = "something_special"
 
+
 def loadClubs():
     with open("clubs.json") as c:
-         listOfClubs = json.load(c)["clubs"]
-         return listOfClubs
+        listOfClubs = json.load(c)["clubs"]
+        return listOfClubs
 
 
 def loadCompetitions():
     with open("competitions.json") as comps:
-         listOfCompetitions = json.load(comps)["competitions"]
-         return listOfCompetitions
+        listOfCompetitions = json.load(comps)["competitions"]
+        return listOfCompetitions
+
+
+def controleDate(competitions):
+    for competition in competitions:
+        format_date_valid = datetime.strptime(competition["date"], FORMAT_DATE)
+        if format_date_valid > datetime.now():
+            competition["valid"] = True
+        else:
+            competition["valid"] = False
+    return
 
 
 competitions = loadCompetitions()
 clubs = loadClubs()
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/showSummary",methods=["POST"])
+
+@app.route("/showSummary", methods=["POST"])
 def showSummary():
     if len(request.form["email"]) == 0:
         message = "No Email : Merci de rentrer une adresse email avant de valider"
@@ -35,8 +50,9 @@ def showSummary():
         clubs = loadClubs()
         for club in clubs:
             if club["email"] == request.form["email"]:
-                club_connected=club
-                return render_template("welcome.html",club=club_connected,competitions=competitions)
+                club_connected = club
+                controleDate(competitions)
+                return render_template("welcome.html", club=club_connected, competitions=competitions)
             else:
                 message = "Unknown Email : L'adresse email n'est pas connu de la base de données"
 
@@ -44,20 +60,21 @@ def showSummary():
 
 
 @app.route("/book/<competition>/<club>")
-def book(competition,club):
+def book(competition, club):
     competitions = loadCompetitions()
     clubs = loadClubs()
-    
+
     foundClub = [c for c in clubs if c["name"] == club][0]
     foundCompetition = [c for c in competitions if c["name"] == competition][0]
+    controleDate(competitions)
     if foundClub and foundCompetition:
-        return render_template("booking.html",club=foundClub,competition=foundCompetition)
+        return render_template("booking.html", club=foundClub, competition=foundCompetition)
     else:
         flash("Something went wrong-please try again")
         return render_template("welcome.html", club=club, competitions=competitions)
 
 
-@app.route("/purchasePlaces",methods=["POST"])
+@app.route("/purchasePlaces", methods=["POST"])
 def purchasePlaces():
     # Charger la liste des compétitions et clubs : permet de passer les tests
     competitions = loadCompetitions()
@@ -65,44 +82,53 @@ def purchasePlaces():
 
     club = [c for c in clubs if c["name"] == request.form["club"]][0]
     competition = [c for c in competitions if c["name"] == request.form["competition"]][0]
-    
-    try:
-        placesRequired = int(request.form["places"])
-        Reponse_valide = True
 
-        # Controle de la réponse utilisateur
-        # Si le nombre de place est supérieur au nombre de point du club
-        if placesRequired > int(club["points"]):
-            message = "Overtaking Club Place : Trop de place acheté par rapport au nombre du club"
-            Reponse_valide = False
-        # Si le nombre de place est supérieur au nombre de place du festival
-        elif placesRequired > int(competition["numberOfPlaces"]):
-            message = "Overtaking Festival Place : Trop de place acheté par rapport au nombre du festival"
-            Reponse_valide = False
-        # Si le nombre de place est supérieur à la limite du max autorisé
-        elif placesRequired > ATHLETES_MAX_COMPETITION:
-            message = "Too Many Athletes : Trop d'athletes inscrit"
-            Reponse_valide = False       
-    except ValueError:
-        # Si le nombre de place est vide
-        message = "No Tickets Purchased : Aucune place achetée"
+    format_date_valid = datetime.strptime(competition["date"], FORMAT_DATE)
+    Reponse_valide = True
+
+    if format_date_valid < datetime.now():
+        competition["valid"] = False
+        message = "Festival Over : Le festival est terminé, donc impossible de réserver"
         Reponse_valide = False
-    
+
+    else:
+        competition["valid"] = True
+        try:
+            placesRequired = int(request.form["places"])
+
+            # Controle de la réponse utilisateur
+            # Si le nombre de place est supérieur au nombre de point du club
+            if placesRequired > int(club["points"]):
+                message = "Overtaking Club Place : Trop de place acheté par rapport au nombre du club"
+                Reponse_valide = False
+            # Si le nombre de place est supérieur au nombre de place du festival
+            elif placesRequired > int(competition["numberOfPlaces"]):
+                message = "Overtaking Festival Place : Trop de place acheté par rapport au nombre du festival"
+                Reponse_valide = False
+            # Si le nombre de place est supérieur à la limite du max autorisé
+            elif placesRequired > ATHLETES_MAX_COMPETITION:
+                message = "Too Many Athletes : Trop d'athletes inscrit"
+                Reponse_valide = False
+        except ValueError:
+            # Si le nombre de place est vide
+            message = "No Tickets Purchased : Aucune place achetée"
+            Reponse_valide = False
+
     if Reponse_valide:
-            # Modification du fichier competition json
-            with open("competitions.json", "w") as competition_json:
-                competition["numberOfPlaces"] = str(int(competition["numberOfPlaces"])-placesRequired)
-                json.dump({"competitions":competitions}, competition_json, sort_keys=False, indent=4 )
-            # Modification du fichier club json
-            with open("clubs.json", "w") as club_json:
-                club["points"] = str(int(club["points"])-placesRequired)
-                json.dump({"clubs": clubs}, club_json, sort_keys=False, indent=4)
-            
-            flash("Great-booking complete!")
-            return render_template("welcome.html", club=club, competitions=competitions)
+        # Modification du fichier competition json
+        with open("competitions.json", "w") as competition_json:
+            competition["numberOfPlaces"] = str(int(competition["numberOfPlaces"])-placesRequired)
+            json.dump({"competitions": competitions}, competition_json, sort_keys=False, indent=4)
+        # Modification du fichier club json
+        with open("clubs.json", "w") as club_json:
+            club["points"] = str(int(club["points"])-placesRequired)
+            json.dump({"clubs": clubs}, club_json, sort_keys=False, indent=4)
+
+        flash("Great-booking complete!")
+        return render_template("welcome.html", club=club, competitions=competitions)
     else:
         # Si il y a une erreur, on reste sur la page booking.html
-        return render_template("booking.html",club=club,competition=competition,message=message)
+        return render_template("booking.html", club=club, competition=competition, message=message)
 
 
 # TODO: Add route for points display
@@ -111,6 +137,7 @@ def purchasePlaces():
 @app.route("/logout")
 def logout():
     return redirect(url_for("index"))
+
 
 # Rajout pour le lancement du programme
 if __name__ == "__main__":
