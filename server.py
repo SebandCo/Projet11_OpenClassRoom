4becd5ff701d1979fe24310184d0070f2d89d0ae
SebandCo <113468072+SebandCo@users.noreply.github.com>
2023-09-11 12:11:51 +0200

    correction affichage erroné

--- server.py
+++ server.py
@@ -91,12 +91,12 @@ def book(competition, club):
     competitions = loadCompetitions()
     clubs = loadClubs()
 
-    foundClub = [c for c in clubs if c["name"] == club][0]
-    foundCompetition = [c for c in competitions if c["name"] == competition][0]
-    controleDate(competitions)
-    if foundClub and foundCompetition:
+    try:
+        foundClub = [c for c in clubs if c["name"] == club][0]
+        foundCompetition = [c for c in competitions if c["name"] == competition][0]
+        controleDate(competitions)
         return render_template("booking.html", club=foundClub, competition=foundCompetition)
-    else:
+    except IndexError:
         flash("Something went wrong-please try again")
         return render_template("welcome.html", club=club, competitions=competitions)
 
--- tests/test_unitaire/test_page_reservation.py
+++ tests/test_unitaire/test_page_reservation.py
@@ -49,3 +49,35 @@ def test_affichage_booking_page_competition_invalid(mocker, client, club_fixture
                            data_club,
                            data_competition,
                            expected_value_booking)
+
+
+def test_affichage_booking_erreur_club_navigation(mocker, client, club_fixture, competition_fixture):
+
+    mocker.patch("server.loadClubs", return_value=club_fixture)
+    mocker.patch("server.loadCompetitions", return_value=competition_fixture)
+
+    data_club = club_fixture[0]
+
+    response = client.get(f"/book/competition_erroné/{data_club['name']}")
+    donnee_decryptee = response.data.decode()
+
+    expected_value_booking = "Something went wrong"
+
+    assert response.status_code == 200
+    assert expected_value_booking in donnee_decryptee
+
+
+def test_affichage_booking_erreur_competition_navigation(mocker, client, club_fixture, competition_fixture):
+
+    mocker.patch("server.loadClubs", return_value=club_fixture)
+    mocker.patch("server.loadCompetitions", return_value=competition_fixture)
+
+    data_competition = competition_fixture[0]
+
+    response = client.get(f"/book/{data_competition['name']}/club_erroné")
+    donnee_decryptee = response.data.decode()
+
+    expected_value_booking = "Something went wrong"
+
+    assert response.status_code == 200
+    assert expected_value_booking in donnee_decryptee
